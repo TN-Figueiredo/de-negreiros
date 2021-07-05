aaebbf3d1a9e1ffd2baf8fe16d064e2bf65eadb6
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2021-07-05 19:33:55 -0300

    Update smallSection.styles.js

--- web/src/components/organisms/sections/smallSection/smallSection.styles.js
+++ web/src/components/organisms/sections/smallSection/smallSection.styles.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import SanityImage from "gatsby-plugin-sanity-image";
 
 export const Container = styled.div`
-  width: 100vw;
   height: 225px;
   background-color: ${(props) =>
     props.background.asset ? "" : props.theme.colors.$blue500};
@@ -12,21 +11,19 @@ export const Overlay = styled.div`
   width: 100vw;
   position: absolute;
   height: 225px;
-  display: flex;
   flex-direction: column;
   background-color: rgba(0, 0, 0, 0.47);
   display: flex;
   justify-content: center;
   align-items: center;
   @media (max-width: 600px) {
-    height: 530px;
   }
 `;
 
 export const StyledSanityImage = styled(SanityImage)`
   position: absolute;
   width: 100vw;
-  max-height: 225px;
+  height: 225px;
   object-fit: cover;
   z-index: -100;
   @media (max-width: 600px) {
