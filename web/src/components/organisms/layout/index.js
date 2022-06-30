e2d90c84ff5d5e1175f6030b1a90650fdc1a23c2
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2022-06-30 13:14:11 -0300

    fix: improve title

--- web/src/components/organisms/layout/index.js
+++ web/src/components/organisms/layout/index.js
@@ -1,21 +1,44 @@
-import React, { useState } from "react";
 import PropTypes from "prop-types";
+import React, { useState } from "react";
+import { Helmet } from "react-helmet";
 import { ThemeProvider } from "styled-components";
+
+import useSiteSettings from "../../../hooks/useSiteSettings.hooks";
+import theme from "../../../theme";
+import Footer from "../../molecules/footer";
 import Header from "../../molecules/header";
 import MobileMenu from "../../molecules/mobileMenu";
-import Footer from "../../molecules/footer";
-
-import theme from "../../../theme";
 import { ThemeContainer } from "./layout.styles";
 
 const Layout = ({ children }) => {
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const { title, description, keywords } = useSiteSettings();
 
   const handleDrawer = () => setDrawerOpen(!drawerOpen);
   return (
     <ThemeProvider theme={theme}>
       <ThemeContainer theme={theme} />
-      <Header handleDrawer={handleDrawer} />
+      <Header handleDrawer={handleDrawer} /><Helmet>
+        <meta charSet="utf-8" />
+        <title>{title || "Advocacia Negreiros"}</title>
+        <meta name="keywords" content={keywords.join() || ""} />
+        <meta
+          name="url"
+          property="og:url"
+          content="https://advocacianegreiros.com.br"
+        />
+        <meta name="type" property="og:type" content="website" />
+        <meta name="title" property="og:title" content={title || "Advocacia Negreiros"} />
+        <meta
+          name="description"
+          property="og:description"
+          content={description || ""}
+        />
+        <meta
+          property="og:image"
+          content="https://cdn.sanity.io/images/impv9vga/production/17c6533df75f89492bd9bd8f3810886c49b5e5f5-8180x4271.png?w=500&q=75&fit=max&auto=format&dpr=2"
+        />
+      </Helmet>
       {drawerOpen ? <MobileMenu /> : children}
       <Footer />
     </ThemeProvider>
