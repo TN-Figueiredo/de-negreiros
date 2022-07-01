9b53b90ce2b56fdc44607a77319f282fb76529b3
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2022-07-01 19:27:23 -0300

    test: whatsapp 2

---
+++ web/src/components/organisms/sections/overlaySection/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+import FloatingWhatsApp from "react-floating-whatsapp";
+
+import { } from "./overlaySection.styles";
+
+const OverlaySection = (props) => {
+    return (
+        <>
+            <FloatingWhatsApp />
+        </>
+    );
+};
+
+OverlaySection.propTypes = {};
+
+export default OverlaySection;
