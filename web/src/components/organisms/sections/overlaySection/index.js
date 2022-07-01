d8d387bbf8e1f7f696cd1ca8d703ede79dacdcb6
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2022-07-01 19:54:54 -0300

    test: whataspp 3

--- web/src/components/organisms/sections/overlaySection/index.js
+++ web/src/components/organisms/sections/overlaySection/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import FloatingWhatsApp from "react-floating-whatsapp";
 
-import { } from "./overlaySection.styles";
-
 const OverlaySection = (props) => {
+    const { whatsappOverlay: { Show: showWhatsapp } } = props;
     return (
         <>
-            <FloatingWhatsApp />
+            {showWhatsapp && <FloatingWhatsApp />}
         </>
     );
 };
