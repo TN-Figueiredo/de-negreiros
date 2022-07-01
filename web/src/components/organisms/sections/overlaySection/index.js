7468086564ef7a13efe463f94e0afd7a31052171
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2022-06-30 21:53:01 -0300

    Update index.js

--- web/src/components/organisms/sections/overlaySection/index.js
+++ web/src/components/organisms/sections/overlaySection/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import FloatingWhatsApp from 'react-floating-whatsapp';
 import useSiteSettings from "../../../../hooks/useSiteSettings.hooks";
 import { renderSectionOptions } from "../components";
 import Photo from "../../../../../static/foto.jpg"
@@ -12,7 +11,6 @@ const OverlaySection = (props) => {
     const { whatsapp } = useSiteSettings();
     return (
         <>
-            {showWhatsapp && <FloatingWhatsApp phoneNumber={`55${whatsapp}`} avatar={Photo} accountName="Felipe Figueiredo" statusMessage="Geralmente responde em até 1 hora" chatMessage={`Olá! 🤝 \nComo posso ajudá-lo?`} placeholder="Digite aqui como podemos ajudá-lo..." />}
         </>
     );
 };
