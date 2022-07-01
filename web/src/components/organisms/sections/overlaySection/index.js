bb6411f58e90ea4d3596e4dd0e7abd388fa646be
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2022-07-01 20:10:41 -0300

    feat: whatsapp in main page

--- web/src/components/organisms/sections/overlaySection/index.js
+++ web/src/components/organisms/sections/overlaySection/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import FloatingWhatsApp from "react-floating-whatsapp";
+import useSiteSettings from "../../../../hooks/useSiteSettings.hooks";
+import Felipe from "../../../../../static/felipe.jpg"
 
 const OverlaySection = (props) => {
     const { whatsappOverlay: { Show: showWhatsapp } } = props;
+    const { whatsapp } = useSiteSettings();
+    console.log("useSiteSettings",useSiteSettings())
     return (
         <>
-            {showWhatsapp && <FloatingWhatsApp />}
+            {showWhatsapp && <FloatingWhatsApp phoneNumber={whatsapp} accountName="Felipe Negreiros" avatar={Felipe} statusMessage="Geralmente responde em até 1 hora" chatMessage={`Olá! 🤝 \nComo podemos ajudar?`} placeholder="Digite aqui como podemos ajudá-lo ..." />}
         </>
     );
 };
