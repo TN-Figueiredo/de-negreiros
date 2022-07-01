b4ff57eee8577651c4017edda5e744071aaf15be
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2022-07-01 20:26:36 -0300

    fix: fix whatsapp numbetr

--- web/src/components/organisms/sections/overlaySection/index.js
+++ web/src/components/organisms/sections/overlaySection/index.js
@@ -1,15 +1,16 @@
-import React from "react";
 import PropTypes from "prop-types";
+import React from "react";
 import FloatingWhatsApp from "react-floating-whatsapp";
-import useSiteSettings from "../../../../hooks/useSiteSettings.hooks";
+
 import Felipe from "../../../../../static/felipe.jpg"
+import useSiteSettings from "../../../../hooks/useSiteSettings.hooks";
 
 const OverlaySection = (props) => {
     const { whatsappOverlay: { Show: showWhatsapp } } = props;
     const { whatsapp } = useSiteSettings();
     return (
         <>
-            {showWhatsapp && <FloatingWhatsApp phoneNumber={whatsapp} accountName="Felipe Negreiros" avatar={Felipe} statusMessage="Geralmente responde em até 1 hora" chatMessage={`Olá! 🤝 \nComo podemos ajudar?`} placeholder="Digite aqui como podemos ajudá-lo ..." />}
+            {showWhatsapp && <FloatingWhatsApp phoneNumber={`55${whatsapp}`} accountName="Felipe Negreiros" avatar={Felipe} statusMessage="Geralmente responde em até 1 hora" chatMessage={`Olá! 🤝 \nComo podemos ajudar?`} placeholder="Digite aqui como podemos ajudá-lo ..." />}
         </>
     );
 };
